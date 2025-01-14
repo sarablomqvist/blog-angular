58ae3c36e62c92ee480bc75fa95a1dc37fd95152
sarablmqvist <sarablmqvist@gmail.com>
2025-01-14 15:24:51 +0100

    fixed about me form

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  name: string = '';
+  phonenr: string = '';
+  mail: string = '';
+  message: string = '';
+
+  sendForm() {
+    if (!this.name || !this.phonenr || !this.mail || !this.message) {
+      alert('Fyll i alla fält tack');
+    } else if (this.name || this.phonenr || this.mail || this.message) {
+      alert('Tack för att du kontaktade mig!');
+    }
+    this.name = '';
+    this.phonenr = '';
+    this.mail = '';
+    this.message = '';
+  }
+}
